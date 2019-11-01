55d1e88ac74f0ccb876b74430e8d661350ef8ced
Dmar <dmmmarrr@gmail.com>
2019-10-31 19:22:35 -0700

    jaw added

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,30 @@
 var mysql = require ("mysql");
-
-var connection = mysql.createConnection({
-host: "localhost",
-port: 3306,
-user: "root",
-password: "",
-database : "burgers_db"
-});
-connection.connect(function(err){
-if (err){
-    console.log("error connecting:" + err.stack);
-    return;
+var connection;
+if (process.env.JAWDB_URL){
+    connection = mysql.createConnection(process.env.JAWDB_URL);
+} else{
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user : 'root',
+        password : 'hacktheplanet',
+        database : 'todoagain_db'
+    })
 }
-console.log("connected as id" + connection.threadId)
-})
+connection.connect();
 module.exports = connection;
+
+// var connection = mysql.createConnection({
+// host: "localhost",
+// port: 3306,
+// user: "root",
+// password: "",
+// database : "burgers_db"
+// });
+// connection.connect(function(err){
+// if (err){
+//     console.log("error connecting:" + err.stack);
+//     return;
+// }
+// console.log("connected as id" + connection.threadId)
+// })
+// module.exports = connection;
